f24c440fce2bdbacb0434a906bb2dbb42e65c20f
findB <justina.mif@gmail.com>
2018-06-06 12:12:07 +0300

    Formos validavimas, pavyzdys is input lauko

--- src/frontend/start.js
+++ src/frontend/start.js
@@ -9,42 +9,52 @@ export default function () {
     document.getElementById('multiplication').innerHTML = `Skaičių ${a} ir ${b} sandauga yra ${multiplication(a, b)}`;
 
 
-    $("#btn1").click(function () {
-        alert("Veikia1");
+    //    $("#btn1").click(function () {
+    //        alert("Veikia1");
+    //    });
+    //    
+    //    $("#btn1").on("click", function () {
+    //        alert("Veikia2");
+    //    });
+    //    
+    //    $("#btn1").on("mouseover", function () {
+    //        $(this).text("Next");
+    //    });
+    //    
+    //    $("div").on("mouseover", function (e) {
+    //        e.stopPropagation();
+    //        $("div").css("border", "none");
+    //        $(this).css("border", "3px solid grey");
+    //    });
+    //    
+    //    $("#btn2").on("click", function () {
+    //        $("#btn1").click();
+    //    });
+    //    
+    //    
+    //    var subHeader = $("<p></p>");
+    //    subHeader.text("subHeader");
+    //    console.log(subHeader);
+    //    subHeader.addClass("h5");
+    //    subHeader.appendTo( $("h1"));
+    //    
+    //    // pakeisti pgr antrastes teksta
+    //    
+    //    
+    //    $("#btn3").on("click", function () {
+    //        $(".h5").css("border", "2px dotted grey").text("Keičiu!");
+    //    });
+
+    $("#btn4").on("click", function () {
+        let name = $("#nameInput").val();
+        alert(`Jūsų vardas ${name}`);
     });
-    
-    $("#btn1").on("click", function () {
-        alert("Veikia2");
-    });
-    
-    $("#btn1").on("mouseover", function () {
-        $(this).text("Next");
-    });
-    
-    $("div").on("mouseover", function (e) {
-        e.stopPropagation();
-        $("div").css("border", "none");
-        $(this).css("border", "3px solid grey");
-    });
-    
-    $("#btn2").on("click", function () {
-        $("#btn1").click();
-    });
-    
-    
-    var subHeader = $("<p></p>");
-    subHeader.text("subHeader");
-    console.log(subHeader);
-    subHeader.addClass("h5");
-    subHeader.appendTo( $("h1"));
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
 
     // Toliau nekeisti
 
