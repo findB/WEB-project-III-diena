706a5844b4521748c6a7b83104abb3d3cf909eb7
findB <justina.mif@gmail.com>
2018-06-06 11:33:02 +0300

    jQ

--- src/frontend/start.js
+++ src/frontend/start.js
@@ -1,13 +1,30 @@
 import multiplication from './functions/multiplication';
 
-export default function() {
+export default function () {
 
     // Nuo čia galima keisti
     const a = Math.floor(Math.random() * 10);
     const b = Math.floor(Math.random() * 10);
 
-    document.getElementById('multiplication').innerHTML
-        = `Skaičių ${a} ir ${b} sąndauga yra ${multiplication(a, b)}`;
+    document.getElementById('multiplication').innerHTML = `Skaičių ${a} ir ${b} sandauga yra ${multiplication(a, b)}`;
+
+
+    $("#btn1").click(function () {
+        alert("Veikia");
+    });
+    
+    $("#btn1").on("click", function () {
+        alert("Veikia");
+    });
+    
+    $("#btn1").on("mouseover", function () {
+        $(this).text("Next");
+    });
+    
+    $("div").on("mouseover", function (e) {
+        e.stopPropagation();
+        $(this).css("border", "3px solid grey");
+    });
 
     // Toliau nekeisti
 
